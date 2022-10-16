6954751c08e4a62bc61ba03dfa4c3f912144dbef
Tam CARRE <kokone@takat.su>
2022-10-16 22:33:36 +0200

    naming

--- src/LC1358/TamCarre.hs
+++ src/LC1358/TamCarre.hs
@@ -3,10 +3,10 @@ module LC1358.TamCarre (abcSubstrs) where
 abcSubstrs :: [Char] -> Int
 abcSubstrs = slidingWindow $ \window -> all (`elem` window) ['a', 'b', 'c']
 
-data SlidingWindow a = SW { count :: Int, wdw :: [a] }
+data SlidingWindow a = SlidingWindow { count :: Int, wdw :: [a] }
 
 slidingWindow :: ([a] -> Bool) -> [a] -> Int
-slidingWindow validWdw list = loop list (SW 0 []) where
+slidingWindow validWdw list = loop list (SlidingWindow 0 []) where
     loop []        = count
     loop (x:xs)    = loop xs . while (validWdw . wdw) (slideWdw xs) . addToWdw x
     addToWdw x sw  = sw {wdw = x:wdw sw}
