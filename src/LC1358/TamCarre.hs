92543e16d4037b0fbc080c1f9cd4e26ebee71cd2
Tam CARRE <kokone@takat.su>
2022-10-16 22:42:12 +0200

    better naming

--- src/LC1358/TamCarre.hs
+++ src/LC1358/TamCarre.hs
@@ -8,7 +8,7 @@ data SlidingWindow a = SlidingWindow { count :: Int, wdw :: [a] }
 slidingWindow :: ([a] -> Bool) -> [a] -> Int
 slidingWindow validWdw list = loop list (SlidingWindow 0 []) where
     loop []        = count
-    loop (x:xs)    = loop xs . while (validWdw . wdw) (slideWdw xs) . addToWdw x
+    loop (x:xs)    = loop xs . while (validWdw . wdw) (shiftWdw xs) . addToWdw x
     addToWdw x sw  = sw {wdw = x:wdw sw}
-    slideWdw xs sw = sw {count = count sw + length xs + 1, wdw = init (wdw sw)}
+    shiftWdw xs sw = sw {count = count sw + length xs + 1, wdw = init (wdw sw)}
     while p        = until (not . p)
