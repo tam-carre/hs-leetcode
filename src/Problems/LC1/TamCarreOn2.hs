ff1930d513bec3ed0ceaec1725a30646338d3622
Tam CARRE <kokone@takat.su>
2022-10-19 16:10:32 +0200

    small refactor of LC1 O(n^2) solution

--- src/Problems/LC1/TamCarreOn2.hs
+++ src/Problems/LC1/TamCarreOn2.hs
@@ -4,14 +4,14 @@ import Data.Foldable (find)
 
 -- | O(n^2)
 twoSum :: [Int] -> Int -> (Int, Int)
-twoSum ints = findMatch intsWithIndices intsWithIndices
+twoSum ints = findMatch intsWithIndices
   where
   intsWithIndices = zip [0..] ints
 
-  findMatch _ [] _ = error "not found"
-  findMatch full (x:xs) target =
-    case find (isMatch target x) full of
-      Nothing    -> findMatch full xs target
+  findMatch [] _ = error "not found"
+  findMatch (x:xs) target =
+    case find (isMatch target x) intsWithIndices of
+      Nothing    -> findMatch xs target
       Just (i,_) -> (fst x, i)
 
   isMatch target (i,x) (j,m) = x + m == target && i /= j
