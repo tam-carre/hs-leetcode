7f93e570932cd8733bec125c14ad19cadbd6ad75
Tam CARRE <kokone@takat.su>
2022-11-05 14:48:26 +0100

    LC3SW formatting

--- src/Problems/LC3/TamCarreSlidingWindow.hs
+++ src/Problems/LC3/TamCarreSlidingWindow.hs
@@ -2,7 +2,7 @@ module Problems.LC3.TamCarreSlidingWindow (lengthOfLongestSubstring) where
 
 import Data.List (nub)
 
--- | This might have time complexity issues, someone should verify this.
+-- | TODO: time complexity verification
 lengthOfLongestSubstring :: String -> Int
 lengthOfLongestSubstring = length . longestValidSubList (\s -> s == nub s)
 
@@ -11,12 +11,12 @@ data SlidingWindow a = SW { answer :: [a], wdw :: [a] }
 -- | Generic sliding window function
 longestValidSubList :: ([a] -> Bool) -> [a] -> [a]
 longestValidSubList validWdw list = loop list (SW [] []) where
-  loop []       = answer
-  loop (x:xs)   = loop xs
-                . updateAnsw
-                . until (validWdw . wdw) shrinkWdw
-                . addToWdw x
-  addToWdw x sw = sw { wdw = wdw sw ++ [x] }
-  shrinkWdw sw  = sw { wdw = tail $ wdw sw }
-  updateAnsw sw = sw { answer = longest (answer sw) (wdw sw) }
-  longest a b   = if length a > length b then a else b
+  loop []             = answer
+  loop (x:xs)         = loop xs
+                      . updateAnsw
+                      . until (validWdw . wdw) shrinkWdw
+                      . addToWdw x
+  addToWdw x (SW a w) = SW a (w ++ [x])
+  shrinkWdw  (SW a w) = SW a (tail w)
+  updateAnsw (SW a w) = SW (longest a w) w
+  longest a b         = if length a > length b then a else b
