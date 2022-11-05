4bd9503d4924b19ab62a26de5cd191be6440cea8
Tam CARRE <kokone@takat.su>
2022-11-05 17:12:03 +0100

    lc1358 improve naming

--- src/Problems/LC1358/TamCarreSlidingWindow.hs
+++ src/Problems/LC1358/TamCarreSlidingWindow.hs
@@ -1,13 +1,14 @@
 module Problems.LC1358.TamCarreSlidingWindow (numberOfSubstrings) where
 
--- | Number of substrings containing a, b and c'
+-- | Number of substrings containing a, b and c
 numberOfSubstrings :: [Char] -> Int
-numberOfSubstrings = slidingWindow $ \window -> all (`elem` window) "abc"
+numberOfSubstrings = validSublists containsAbc where
+  containsAbc str = all (`elem` str) "abc"
 
-data SlidingWindow a = SlidingWindow { count :: Int, wdw :: [a] }
+data SlidingWindow a = SW { count :: Int, wdw :: [a] }
 
-slidingWindow :: ([a] -> Bool) -> [a] -> Int
-slidingWindow validWdw list = loop list (SlidingWindow 0 []) where
+validSublists :: ([a] -> Bool) -> [a] -> Int
+validSublists validWdw list = loop list (SW 0 []) where
   loop []        = count
   loop (x:xs)    = loop xs . while (validWdw . wdw) (shiftWdw xs) . addToWdw x
   addToWdw x sw  = sw {wdw = x:wdw sw}
