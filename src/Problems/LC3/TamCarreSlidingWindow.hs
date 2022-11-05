65b669e190ea9d7a0ee33659083660cdbe15bba5
Tam CARRE <kokone@takat.su>
2022-11-05 16:57:47 +0100

    improve lc3 sliding window

--- src/Problems/LC3/TamCarreSlidingWindow.hs
+++ src/Problems/LC3/TamCarreSlidingWindow.hs
@@ -1,23 +1,19 @@
 module Problems.LC3.TamCarreSlidingWindow (lengthOfLongestSubstring) where
 
-import qualified Data.Set as Set
+import Data.List (nub)
+
+data SlidingWindow a = SW { longest :: [a], wdw :: [a] }
 
 -- | TODO: time complexity verification
 lengthOfLongestSubstring :: String -> Int
-lengthOfLongestSubstring = length . longestSubList withNoDupes where
-  withNoDupes xs = length xs == length (Set.fromList xs)
+lengthOfLongestSubstring str = loop str (SW "" "") where
+  loop []     = length . longest
+  loop (x:xs) = loop xs
+              . updateLongest
+              . until (hasNoDupe . wdw) shrinkWindow
+              . addToWindow x
 
-data SlidingWindow a = SW { answer :: [a], wdw :: [a] }
-
--- | Generic sliding window function
-longestSubList :: ([a] -> Bool) -> [a] -> [a]
-longestSubList validSublist list = loop list (SW [] []) where
-  loop []             = answer
-  loop (x:xs)         = loop xs
-                      . updateAnsw
-                      . until (validSublist . wdw) shrinkWdw
-                      . addToWdw x
-  addToWdw x (SW a w) = SW a (w ++ [x])
-  shrinkWdw  (SW a w) = SW a (tail w)
-  updateAnsw (SW a w) = SW (longest a w) w
-  longest a b         = if length a > length b then a else b
+  addToWindow x (SW l w) = SW l (x:w)
+  shrinkWindow  (SW l w) = SW l (init w)
+  hasNoDupe xs           = xs == nub xs
+  updateLongest (SW l w) = SW (if length l > length w then l else w) w
